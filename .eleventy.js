08a6b803a4226de7fdf07c90906f60d0b2160e0f
Filip Chalupa <chalupa.filip@gmail.com>
2021-09-07 01:24:20 +0200

    Add syntax highlighting support

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/static')
+
+	eleventyConfig.addPlugin(syntaxHighlight)
 
 	return {
 		dir: {
